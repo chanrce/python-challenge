fed7d1de98b57cd93c03a161ce36312f43bc505b
Rachel Chan <RachelChan@Rachels-MacBook-Pro-2.local>
2020-09-25 17:04:09 -0400

    added comments for txt file reminder

--- PyBank/Solved/main.py
+++ PyBank/Solved/main.py
@@ -9,3 +9,22 @@ budget_data = os.path.join("..", "Resources", "budget_data.csv")
 with open(budget_data) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
+    for row in csvreader:
+        title.append(row[1])
+        price.append(row[4])
+        subscribers.append(row[5])
+        reviews.append(row[6])
+
+        percent = round(int(row[6])/int(row[5]),2)
+        review_percent.append(percent)
+
+        new_length = row[9].split(" ")
+        length.append(float(new_length[0]))
+
+cleaned_csv = zip(title,price,subscribers,reviews,review_percent,length)
+output_file = os.path.join("web_final.csv")
+
+#BELOW CONVERT TO TXT FILE!!!!!!!!!!!!!
+#with open(output_file, "w") as datafile:
+    #writer = csv.writer(datafile)
+    #writer.writerow(["Title", "Course Price", "Subscribers,"Reviews Left", "Percent of Review", ])
