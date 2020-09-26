c5110aa447ebc14c87d23e2e090c3220075a31f6
Rachel Chan <RachelChan@Rachels-MacBook-Pro-2.local>
2020-09-26 16:42:16 -0400

    percentchange

--- PyPoll/Solved/main.py
+++ PyPoll/Solved/main.py
@@ -11,6 +11,7 @@ candidatename =[]
 
 candidatedict = {}
 
+totalvotes = 0
 #candidates =[]
 
 with open(election_data) as csvfile:
@@ -18,6 +19,7 @@ with open(election_data) as csvfile:
     next(csvreader)
 
     for row in csvreader:
+        totalvotes=totalvotes+1
         voterid.append(row[0])
         #candidates.append(row[2])
         if row[2] not in candidatename:
@@ -26,10 +28,22 @@ with open(election_data) as csvfile:
         candidatedict[row[2]]=candidatedict[row[2]]+1
         #dictionary to store data, created from row reader
         #candidatedict = {"county": row[1], "candidate": row[2]}
+    percent_dictionary = {}
+    maximumvotes = 0
+    winner = ""
 
+    #runs a for loop on percent dict, runs through candidate's votes in dict to determine winner
+    for x in candidatedict:
+        percent_dictionary[x]=round((candidatedict[x]/totalvotes)*100)
+        votes = candidatedict.get(x)
+        if votes > maximumvotes:
+            maximumvotes = votes
+            winner = x
 
 
 #prints total number of votes cast
 print(str(len(voterid)))
 print(candidatename)
 print(candidatedict)
+print(percent_dictionary)
+print(winner)
