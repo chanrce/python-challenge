d3b8becbf8730ce6d4ae4534b5d335f7a9c4ad21
Rachel Chan <RachelChan@Rachels-MacBook-Pro-2.local>
2020-09-26 23:30:07 -0400

    finished greatest increase in profits

--- PyBank/Solved/main.py
+++ PyBank/Solved/main.py
@@ -7,12 +7,14 @@ budget_data = os.path.join("..", "Resources", "budget_data.csv")
 
 #Lists to store data
 months = []
-profitlosses = []
 
-#sumchanges = []
+
 profitinc = []
 profitdec = []
 change_from_previous = []
+
+#dictionary to store dates with corresponding changes
+changedict = {}
 
 #make variables
 totalprofitlosses = 0
@@ -26,37 +28,35 @@ with open(budget_data) as csvfile:
 
     for row in csvreader:
         months.append(row[0])
-        profitlosses.append(row[1])
         totalprofitlosses += int(row[1])
 
         change = int(row[1]) - previous
         change_from_previous.append(change)
         previous = int(row[1])
-    
-    avgchange = sum(change_from_previous[1:])/(len(change_from_previous)-1)
-
         
-        #sumchanges += int(changes)
 
-       # subscribers.append(row[5])
-        #reviews.append(row[6])
+candidatedict = dict(zip(months, change_from_previous))
+#max(candidatedict(change_from_previous))
 
-        #percent = round(int(row[6])/int(row[5]),2)
-        #review_percent.append(percent)
+Keymax = max(candidatedict, key=candidatedict.get) 
+print("max is: " + str(Keymax) + ":" + str(max(change_from_previous)))
 
-        #new_length = row[9].split(" ")
-        #length.append(float(new_length[0]))
-
+    
+#calculation of average change
+avgchange = sum(change_from_previous[1:])/(len(change_from_previous)-1)
+        
 #counting number of months
 print(str(len(months)))
+
+#printing results
 print(totalprofitlosses)
 print(avgchange)
 
 
-#adding up the profits/losses
-
-
-
+#for range month in months 
+#if max(change_from_previous):
+#    print(month)
+#print(min(change_from_previous))
 
 #cleaned_csv = zip(title,price,subscribers,reviews,review_percent,length)
 #output_file = os.path.join("web_final.csv")
