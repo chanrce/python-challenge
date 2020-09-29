4e1c958f3c3b4ce4811322ae3c1ab4fa967f31cf
Rachel Chan <RachelChan@Rachels-MacBook-Pro-2.local>
2020-09-29 11:40:30 -0400

    added comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -16,19 +16,22 @@ with open(election_data) as csvfile:
     csv_header = next(csvreader)
 
     for row in csvreader:
+        #Using a loop to count total votes
         totalvotes=totalvotes+1
+        #Using the list candidate name and referencing it to make sure the new names are not currently in the list
         voterid.append(row[0])
-        #candidates.append(row[2])
         if row[2] not in candidatename:
             candidatename.append(row[2])
             candidatedict[row[2]]=0
+        #counting the total number of votes for each candidate, adding 1 vote for every row for the corresponding candidate
         candidatedict[row[2]]=candidatedict[row[2]]+1
         #dictionary to store data, created from row reader
     percent_dictionary = {}
     maximumvotes = 0
     winner = ""
 
-    #runs a for loop on percent dict, runs through candidate's votes in dict to determine winner
+   
+    #runs a for loop on percent dict, runs through candidate's votes in dict to determine winner!
     for x in candidatedict:
         percent_dictionary[x]=round((candidatedict[x]/totalvotes)*100)
         votes = candidatedict.get(x)
@@ -37,7 +40,7 @@ with open(election_data) as csvfile:
             winner = x
 
 #printing analysis
-PyPollAnalysis= (f"Election Results \n------------------------------------\nTotal Votes: {str(len(voterid))} \n------------------------------------ \nTotal Votes by Candidate: \n{candidatedict} \nPercent of Votes by Candiates: \n{percent_dictionary} \n------------------------------------ \nWinner: {winner}\n------------------------------------ ")
+PyPollAnalysis= (f"Election Results \n------------------------------------\nTotal Votes: {str(totalvotes)} \n------------------------------------ \nTotal Votes by Candidate: \n{candidatedict} \nPercent of Votes by Candiates: \n{percent_dictionary} \n------------------------------------ \nWinner: {winner}\n------------------------------------ ")
 print(PyPollAnalysis)
 
 #exporting analysis to text file
